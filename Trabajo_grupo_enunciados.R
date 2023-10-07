7a175341557a410c79d5e6bc6bc73966893acadb
Marta Rodriguez Hebles <martarodriguezhebles@Air-de-Marta-2.technicolor.net>
2023-10-07 12:41:43 +0200

    creación fichero unicamente con los enunciados

---
+++ Trabajo_grupo_enunciados.R
@@ -0,0 +1,21 @@
+###############################################################################
+# PRIMER TRABAJO TEORÍA DE LA DECISIÓN 
+# Métodos de incertidumbre                                           
+# Enunciados inventados por cada alumno
+###############################################################################
+
+########## Luis Ocaña
+# Luis se va de vacaciones de Mikonos y quiere alquilar un coche durante un día
+# para recorrerse la isla. Estaba pensando alquilar un coche pequeño, como un
+# fiat 500, encontrando las siguientes 3 empresas con este tipo de vehículos:
+# Avis, que por 1 día le cobra 50€, Europcar por 65€ y Cicar por 80€.
+# Todas las empresas tienen una política de seguros muy diferentes.
+# En Avis no incluye ningún seguro ni asistencia en carretera, cobrándole 25€ si
+# pincha una rueda, 60€ si rompe la luna del coche y 350€ en caso de que el coche
+# quede siniestro. En Europcar tiene un seguro básico, que cubre una parte de los
+# daños; si se le pincha la rueda del coche le cobran 15€, 25€ si se le rompe la
+# luna del coche y 185€ en caso de siniestro. En cambio, en Cicar cuenta con un
+# seguro a todo riesgo sin franquicia.
+# ¿Qué empresa debería escoger Luis si quiere ahorrarse el máximo dinero
+# posible/piensa que todo le va a ir bien?
+# ¿Y si fuera al revés y tuviera siempre "mala suerte"?
