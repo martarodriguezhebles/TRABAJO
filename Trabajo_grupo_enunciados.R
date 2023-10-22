ebab12c2319150596fdada905d9d87e78b3cfc7c
luciars14 <luciaromerosanchez12@gmail.com>
2023-10-22 14:42:02 +0200

    Añadir mi problema al fichero Trabajo_grupo_enunciado

---
+++ Trabajo_grupo_enunciados.R
@@ -0,0 +1,54 @@
+###############################################################################
+# PRIMER TRABAJO TEORÍA DE LA DECISIÓN 
+# Métodos de incertidumbre                                           
+# Enunciados inventados por cada alumno
+###############################################################################
+
+########## Luis Ocaña
+# Luis se va de vacaciones de Mikonos y quiere alquilar un coche durante un día
+# para recorrerse la isla. Estaba pensando alquilar un coche pequeño, como un
+# fiat 500, encontrando las siguientes 3 empresas con este tipo de vehículos:
+# Avis, que por 1 día le cobra 50€, Europcar por 65€ y Cicar por 80€.
+# Todas las empresas tienen una política de seguros muy diferentes.
+# En Avis no incluye ningún seguro ni asistencia en carretera, cobrándole 25€ si
+# pincha una rueda, 60€ si rompe la luna del coche y 350€ en caso de que el coche
+# quede siniestro. En Europcar tiene un seguro básico, que cubre una parte de los
+# daños; si se le pincha la rueda del coche le cobran 15€, 25€ si se le rompe la
+# luna del coche y 185€ en caso de siniestro. En cambio, en Cicar cuenta con un
+# seguro a todo riesgo sin franquicia.
+# ¿Qué empresa debería escoger Luis si quiere ahorrarse el máximo dinero
+# posible/piensa que todo le va a ir bien?
+# ¿Y si fuera al revés y tuviera siempre "mala suerte"?
+
+
+######## Marta Rodríguez Hebles
+# Una empresa de venta de electrodomésticos desea contartar un proveedor para el próximo año,
+# de manera que le salga lo más rentable posible. Todos los proveedores tienes un precio fijo 
+# inicial para un pedido de 1000 unidades, dependiendo de la marca, y dicho precio aumenta 
+# por cada 100 unidades extra que la empresa solicite fuera de plazo. 
+# Tiene tres opciones, el proveedor A le cobra 500 u.m.por el primer pedido, el B 450 u.m. 
+# y el C 700 u.m.. si añade 100 unidades mas, el proveedor A le cobrara 200 u.m. de suplemento, 
+# el B 250 u.m. y el C 100 u.m.. Mientras que si añade otras 100 unidades mas el proveedor A 
+# le sumará 150 u.m, el B 200 u.m. y el C nada.
+# ¿Cuál es la opción más rentable si espera muchas ventas este año? ¿Y si tiene una previsión negativa?
+
+
+######## Lucía Romero Sánchez
+# Sergio es un ingeniero que trabaja en una empresa aeronáutica sevillana. 
+# Para firmar un contrato de producción de nuevos aviones, debe viajar a Madrid 
+# y alojarse durante varios días allí, por lo que debe llevarse una maleta de 
+# gran tamaño. Para su viaje está consultando 3 aerolíneas de manera que su 
+# desplazamiento le salga lo más económico posible. 
+
+# En cuanto al precio de los billetes, la aerolínea Ryanair ofrece el viaje de 
+# ida por 25€ y la posibilidad de reservar por adelantado el viaje de vuelta por 
+# 40€. La aerolínea Vueling, ofrece el viaje de ida por 39€ y el viaje de vuelta
+# por 33€. Finalmente, Iberia ofrece el viaje de ida por 60€ y el viaje de vuelta 
+# por 10€.
+
+# Como él debe facturar maleta, las compañías le exigen pagar un suplemento de 
+# 45€ (Ryanair), de 29€ (Vueling) y de 25€ (Iberia).
+
+#¿En qué aerolínea debería comprar Sergio su billete de manera que su desplazamiento 
+# le salga lo más económico posible?
+  
